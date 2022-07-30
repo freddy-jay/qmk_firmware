a1ffdade934eef5fe2f6a07b1f75c25ee39504bd
freddy-jay <federicogiajlevra@icloud.com>
2022-07-30 15:22:58 +0200

    make mouse layer transparent

--- keyboards/planck/keymaps/freddy-jay/keymap.c
+++ keyboards/planck/keymaps/freddy-jay/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN,   KC_RGHT
 ),
 
 /* Colemak
@@ -151,10 +151,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,  _______
 ),
 [9] = LAYOUT_planck_grid(
-      KC_TAB,   KC_NO,     KC_NO,     KC_NO,    KC_NO,      KC_NO,          KC_NO,    KC_NO,      KC_MS_U,    KC_NO,      KC_WH_D,    _______,  
-      TG(9),    KC_A,      KC_S,      KC_BTN2,  KC_BTN1,    KC_NO,          KC_NO,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_U,    KC_NO,  
-      KC_LSFT,  KC_Z,      KC_X,      KC_C,     KC_V,       KC_NO,          KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      _______,   
-      KC_NO,    KC_LCTL,   KC_LALT,   KC_LGUI,  _______,    _______,        _______,  _______,    _______,    _______,    _______,    _______
+      KC_TAB,   _______,   _______,   _______,  _______,    _______,        _______,  _______,    KC_MS_U,    _______,    KC_WH_D,    _______,  
+      TG(9),    KC_A,      KC_S,      KC_BTN2,  KC_BTN1,    _______,        _______,  KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_U,    _______,  
+      KC_LSFT,  KC_Z,      KC_X,      KC_C,     KC_V,       _______,        _______,  _______,    _______,    _______,    _______,    _______,   
+      _______,  KC_LCTL,   KC_LALT,   KC_LGUI,  _______,    _______,        _______,  _______,    _______,    _______,    _______,    _______
   ),
 };
 
